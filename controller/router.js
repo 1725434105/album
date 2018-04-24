8b76b2cf3bafab5144e313c3174f6729bebd6cae
panju <1725434105@qq.comcls>
2018-04-25 01:00:34 +0800

    增加文件上传功能 同时限制上传图片的大小

--- controller/router.js
+++ controller/router.js
@@ -1,4 +1,8 @@
 const filesOperator = require('../models/file')
+const formidable = require("formidable")
+const path = require("path")
+const fs = require("fs")
+const sd = require('silly-datetime');
 
 function showIndex(req, res) {
   filesOperator.getAllDirection(function(err,album){
@@ -28,7 +32,60 @@ function showAlbum(req, res) {
 // module.exports = {
 //   showIndex
 // }
+
+
+function showUp(req, res) {
+    filesOperator.getAllDirection(function(err,album){
+      if(err) {
+        res.send(err);
+        return;
+      }
+      res.render("up", {"albums": album})
+    })
+}
+// 上传表单
+function doPost(req, res, next) {
+  console.log(req.url)
+  var form = new formidable.IncomingForm();
+  form.uploadDir = path.resolve(__dirname,'../temp')
+  form.parse(req, function(err, fields, files) {
+    // res.writeHead(200, {'content-type': 'text/plain'});
+    // res.write('received upload:\n\n');
+    // res.end(util.inspect({fields: fields, files: files}));
+    console.log(fields)
+    console.log(files)
+    // 改名
+    if(err) {
+      next();
+      return;
+    }
+    // 文件名的重命名采用时间+随机+拓展名
+    var randomTime = sd.format(new Date(),"YYYYMMDDHHmmss") + parseInt(Math.random()*899999);
+    var extName = path.extname(files.file.name);
+    var wenjianjia = fields.wenjianjia;
+    var oldpath = files.file.path;
+    var newpath = path.resolve(__dirname,"../uploads/"+wenjianjia+"/"+randomTime+extName);
+    console.log(files.file.size)
+    if(parseInt(files.file.size) > 1024) {
+      res.send("图片尺寸应小于1M");
+      fs.unlink(files.file.path,err=> {
+        // 删除文件失败
+      })
+      return
+    }
+    fs.rename(oldpath, newpath, function(err){
+      if(err) {
+        res.send("改名失败")
+        return;
+      }
+      res.send("上传成功")
+    })
+  });
+}
+
 module.exports= {
   showIndex,
-  showAlbum
+  showAlbum,
+  showUp,
+  doPost,
 }
--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(express.static('./public'))
 app.use(express.static('./uploads'))
 app.get('/',router.showIndex)
 
+app.get('/up', router.showUp)
+app.post('/up', router.doPost)
 app.get('/:album', router.showAlbum)
 app.use(function(req, res){
   console.log(req.url)
